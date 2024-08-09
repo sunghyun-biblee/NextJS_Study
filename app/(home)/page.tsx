dabb0411a9151db188a1135962816b4e6daa980c
sunghyun1014 <sunghyun1014@naver.com>
2024-08-09 22:33:51 +0900

    MetaData, Dynamic Routes study

---
+++ app/(home)/page.tsx
@@ -0,0 +1,13 @@
+import Navigation from "../../components/navigation";
+
+export const metadata = {
+  title: "Home",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}
--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,10 +1,12 @@
 import Navigation from "../../components/navigation";
 import { Avatar } from "./components/Avatar";
 
+export const metadata = {
+  title: "About-us",
+};
 export default function page() {
   return (
     <div>
-      <Navigation></Navigation>
       <h1>
         About Us <Avatar />
       </h1>
