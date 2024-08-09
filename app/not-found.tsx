8ca2b9bf1a2267ab1497e72574c29aa86cc97806
sunghyun1014 <sunghyun1014@naver.com>
2024-08-09 22:33:24 +0900

    hydrate,use client study

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,12 @@
+import { Metadata } from "next";
 import Navigation from "./../components/navigation";
+export const metadata: Metadata = {
+  title: "Not found",
+};
 
 export default function Notfound() {
   return (
     <div>
-      <Navigation />
       <h1>Not Found!</h1>
     </div>
   );
