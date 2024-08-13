b7ea8bf6d747d892d5eb21e098bf7dbc9b3cb3a0
sunghyun1014 <sunghyun1014@naver.com>
2024-08-14 01:45:49 +0900

    server component를 통해 fethcing하고 loading 처리를 배워봄

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,46 @@
-import Navigation from "../../components/navigation";
+import Link from "next/link";
 
 export const metadata = {
-  title: "Home",
+  title: "home",
 };
 
-export default function Page() {
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies";
+const getMovies = async () => {
+  const response = await fetch(API_URL);
+  const json = response.json();
+  return json;
+};
+export default async function HomePage() {
+  // 컴포넌트 앞에 async를 작성하는 이유는
+  // Next.js가 이 component는 await을 해야하기 때문
+  const movies = await getMovies();
   return (
     <div>
       <h1>hello</h1>
+      {movies.map((movie) => (
+        <li key={movie.id}>
+          <Link href={`/movies/${movie.id}`}>{movie.title}</Link>
+        </li>
+      ))}
+      {/* {JSON.stringify(movies)} */}
     </div>
   );
 }
+
+// 사용자가 웹사이트에 도착하는 순간
+// next.js는 즉시 사용자에게 로딩 상태를 전달한다
+// 하지만 streaming을 사용하여, next.js는 기본적으로 우리의 페이지를 작은 html 부분으로 나눠서 준비된 html부분을 브라우저에게 줄 수 있다.
+// 예시로, navigation bar와 loading component  요소
+// 위 2가지 컴포넌트들은 사용자에게 보여질 준비가 되어있기 떄문에, 해당 html 부분들은 브라우저에 즉시 전달된다.
+
+// 그리고 브라우저에게 백엔드에서 작업이 마무리되지 않아서, 기다려줘야 한다고 하는 것
+// await이 끝나면 브라우저에게 마지막 html 부분을 전달하게 되고,
+// 그럼 이때, 프레임워크가 loading component를 await된 component로 바꿔주게 된다.
+
+// *. 프레임워크가 우리의 페이지를 작은 html 부분으로 나누고, 준비된 부분들을 하나씩 보여준다는 것
+
+/*
+1. <Loading/>
+2. const html= await Homepage()
+3. isLoading ? <Loading/>: html
+*/
--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,48 @@
-export default function MovieDetail({
+import { API_URL } from "../../../(home)/page";
+
+export const metadata = {
+  title: "MovieDetail",
+};
+async function getMovie(id: string) {
+  console.log(`Fetching Movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const res = await fetch(`${API_URL}/${id}`);
+  return res.json();
+}
+async function getVideos(id: string) {
+  console.log(`Fetching Movies: ${Date.now()}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  const res = await fetch(`${API_URL}/${id}/videos`);
+  return res.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  console.log(id);
-  return <h1>Movie {id}</h1>;
+  console.log("==============");
+  console.log("start Fetching");
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  console.log("end Fetching");
+  return <h1>{movie.title}</h1>;
 }
+
+// api 요청 작업이 2개 이상일때 하나의 작업시간이 오래걸리고, 다른작업이 빨라도 평균작업 시간은 오래걸리게된다.
+// 만약 비디오데이터는 1초만에 받아왔으나, 영화 정보를 받아오는데 20초가 걸리게되면 총 작업시간이 21초가 되고, await을 통해 영화정보를 가져온 후 비디오 데이터를 가져오기 때문에 순차적으로 해결하는데 많은 시간이 소요된다.
+
+// 이를 병렬적으로 작업하여, 최적화 해보자
+
+/*
+A. getMovie, getVideos 2개의 작업을 promise.all을 사용하여 두 작업 모두 동시에 await을 적용한다.
+_ await Promise.all([getMovie(id),getVideos(id)])  => 두 작업을 동시에 시작
+위와 같이 작성하게되면 promise.all이 배열에 있는 작업들 모두 await을 적용하게 되고,
+각 작업이 끝나면 데이터가 반환된다.
+1. Promise.all([ await getMovie(id), await getVideos(id)]) 
+2. Promise.all([ movie, await getVideos(id)]) 
+3. Promise.all([ movie, videos]) 
+=> prmise.all은 결과값을 이루어진 배열을 가지게 됨
+
+B. 모든 작업이 끝나게되면 , promise.all이 결과값으로 이루어진 배열을 반환한다.
+
+*/
